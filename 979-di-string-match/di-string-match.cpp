fddb9fb6b41aaf5bb633da7e94ab55091abce15f
abhi-guntuk22 <155354534+abhi-guntuk22@users.noreply.github.com>
2025-12-23 12:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (31.82%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int low = 0, high = s.size();
+        vector<int> ans;
+
+        for (char c : s) {
+            if (c == 'I')
+                ans.push_back(low++);
+            else
+                ans.push_back(high--);
+        }
+        ans.push_back(low);  // last remaining number
+        return ans;
+    }
+};
