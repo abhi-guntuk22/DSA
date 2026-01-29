d0990110ce26dc8c14367c7d627ad394e70063f2
abhi-guntuk22 <155354534+abhi-guntuk22@users.noreply.github.com>
2026-01-29 17:03:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (98.52%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
     
-    int n =s.size();
     int first=0,sec=0;
     vector<bool>cnt(256,0);
     int len=0;
-    while(sec<n)
+   while(sec<s.size())
+   {
+    while(cnt[s[sec]])    //sec pr ager rep chr he 
     {
-        //repeating remove kro
-
-        while(cnt[s[sec]])
-        {
-            cnt[s[first]]=0;
-            first++;
-        } 
-        cnt[s[sec]]=1;
-        len=max(len ,sec-first+1);
-        sec++;
-        
-
+     cnt[s[first]]=0;   // to windo left se choti kro                 
+     first++ ;        // till wo char windo se baher ho jaye 
     }
-    return len;
+
+    cnt[s[sec]]=1;  // ager rep nhi he to sec badate reh 
+
+    len=max(len,sec-first+1); 
+    sec++;
+
+   }
+   return len;
+
     }
     //     int lengthOfLongestSubstring(string s) {
 //     int n = s.size();
