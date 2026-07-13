d0b25a07e057501d4d58fd6bb4707fe45d13d6cb
abhi-guntuk22 <155354534+abhi-guntuk22@users.noreply.github.com>
2026-07-13 21:19:21 +0530

    Time: 6 ms (66.22%) | Memory: 10.8 MB (94.20%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    
-    int first=0,sec=0;
-    vector<bool>cnt(256,0);
-    int len=0;
-   while(sec<s.size())
-   {
-    while(cnt[s[sec]])    //sec pr ager rep chr he 
-    {
-     cnt[s[first]]=0;   // to windo left se choti kro                 
-     first++ ;        // till wo char windo se baher ho jaye 
-    }
+    int l=0;
+    int maxlen=0;
+// unordered_map<char,int>mp;
 
-    cnt[s[sec]]=1;  // ager rep nhi he to sec badate reh 
-
-    len=max(len,sec-first+1); 
-    sec++;
-
-   }
-   return len;
-
-    }
-    //     int lengthOfLongestSubstring(string s) {
-//     int n = s.size();
-//     int maxLen = 0;
-
-//     // start index
-//     for (int i = 0; i < n; i++) {
-
-//         vector<bool> used(256, false);
-
-//         // end index
-//         for (int j = i; j < n; j++) {
-//             if (used[s[j]]) {
-//                 break;  // duplicate found
-//             }
-//             used[s[j]] = true;
-//             maxLen = max(maxLen, j - i + 1);
+//     for(int r=0;r<s.size();r++)
+//     {
+//         if(mp.find(s[r])!=mp.end())
+//         {
+          
+//           l=max(l,mp[s[r]]+1);
 //         }
+//         mp[s[r]]=r;
+//         maxlen=max(maxlen,r-l+1);
+
 //     }
-//     return maxLen;
-// }
+  vector<int>mp(128,-1);
+  for(int r=0;r<s.size();r++)
+  {
+    if(mp[s[r]]>=l)
+    {
+        l=mp[s[r]]+1;
+        
+    }
+    mp[s[r]]=r;
+    maxlen=max(maxlen,r-l+1);
+  }
+    return maxlen;
+    
+   }
 
 };
